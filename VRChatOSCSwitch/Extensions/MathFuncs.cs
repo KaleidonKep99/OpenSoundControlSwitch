53a065fb374d99fb67b69c0b3d70bedcda2a3497
bree <kaleidonkep99@outlook.com>
2022-05-19 15:17:21 +0200

    MathFuncs etc

---
+++ VRChatOSCSwitch/Extensions/MathFuncs.cs
@@ -0,0 +1,40 @@
+﻿namespace VRChatOSCSwitch
+{
+    public class MathFuncs
+    {
+        // Pass a 0.0 to X.X (default 1.0) float value, and turn it into a 0 to X int value
+        public int FtoI(float Value, int MaxI, float? MaxF = 1.0f)
+        {
+            return (int)(Value >= MaxF ? MaxI : Value * (MaxI + 1));
+        }
+
+        // Pass a value, then the minimum and maximum range for it,
+        // and the function will spit out a percentage from 0 to 100 (int)
+        public int ItoRi(int Value, int MinI, int MaxI)
+        {
+            return (Value - MinI) * 100 / (MaxI - MinI);
+        }
+
+        // Pass a value, then the minimum and maximum range for it,
+        // and the function will spit out a percentage from 0.0 to 1.0 (float)
+        public float ItoRf(int Value, float MinF, float MaxF)
+        {
+            return (Value - MinF) * 1.0f / (MaxF - MinF);
+        }
+
+        // Linear interpolation, useful when you want to get a 0.0 to 1.0 float value and translate it
+        // to a specific range of your choosing (e.g. 0.0 - 1.0 to 1700.0 to 6500.0, for the light temperature,
+        // which you will then round by casting it to an int; (int)6500.0 -> 6500K)
+        public float Lerp(float Value, float MinF, float MaxF)
+        {
+            return MinF * (1 - Value) + MaxF * Value;
+        }
+
+        // Round an integer number to an even number
+        // (Why isn't this built into the Math classas one function already?)
+        public decimal RoundNum(int Value)
+        {
+            return Math.Round((decimal)Value / 2, MidpointRounding.AwayFromZero) * 2;
+        }
+    }
+}
---
+++ VRChatOSCSwitch/Extensions/OSC/OSCAddresses.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+
+namespace VRChatOSCSwitch
+{
+    public partial class OSCAddress
+    {
+        // The first part of the address (e.g. /avatar/parameters)
+        [JsonProperty("Address")]
+        public string Address { get; set; }
+
+        // The array containing all the parameters stored under that address (e.g. MouthUpper, MouthLower etc.)
+        [JsonProperty("Parameters")]
+        public string[] Parameters { get; set; }
+
+        // Unused
+        public OSCAddress() { }
+
+        // Used to create the example JSON
+        public OSCAddress(string A, string[] P)
+        {
+            Address = A;
+            Parameters = P;
+        }
+    }
+
+    public partial class OSCAddressHTTP
+    {
+        // The address (e.g. /avatar/parameters/param)
+        [JsonProperty("Address")]
+        public string Address { get; set; }
+
+        public OSCAddressHTTPItem[] Vars { get; set; }
+
+        // Used to create the example JSON
+        public OSCAddressHTTP(string A, OSCAddressHTTPItem[] Va)
+        {
+            Address = A;
+            Vars = Va;
+        }
+    }
+
+    public partial class OSCAddressHTTPItem
+    {
+        // The address (e.g. /avatar/parameters/param)
+        [JsonProperty("Constant")]
+        public string VarName { get; }
+
+        // The variable type
+        [JsonProperty("VarType")]
+        public Type VarType { get; }
+
+        // The address (e.g. /avatar/parameters/param)
+        [JsonProperty("Constant")]
+        public bool Constant { get; }
+
+        // The address (e.g. /avatar/parameters/param)
+        [JsonProperty("Value")]
+        public object? Value { get; }
+
+        // The minimum for the value (if required)
+        [JsonProperty("MinValue")]
+        public object? MinValue { get; }
+
+        // The maximum for the value
+        [JsonProperty("MaxValue")]
+        public object? MaxValue { get; }
+
+        public OSCAddressHTTPItem(string VN, string VT, object? Const, object? MiV, object? MaV)
+        {
+            if (Const != null)
+            {
+                Constant = true;
+                Value = Const;
+
+                switch (Const)
+                {
+                    case float f:
+                    case double d:
+                        VarType = typeof(float);
+                        break;
+                    case int i:
+                    case long l:
+                        VarType = typeof(int);
+                        break;
+                    case char c:
+                    case string s:
+                        VarType = typeof(string);
+                        break;
+                    case bool b:
+                        VarType = typeof(bool);
+                        break;
+                    default:
+                        VarType = typeof(object);
+                        break;
+                }
+            }
+            else
+            {
+                Constant = false;
+
+                switch (VT)
+                {
+                    case "float":
+                        VarType = typeof(float);
+                        break;
+                    case "int":
+                        VarType = typeof(int);
+                        break;
+                    case "string":
+                        VarType = typeof(string);
+                        break;
+                    case "boolean":
+                        VarType = typeof(bool);
+                        break;
+                    default:
+                        VarType = typeof(object);
+                        break;
+                }
+            }
+
+            MinValue = MiV;
+            MaxValue = MaV;
+        }
+    }
+}
