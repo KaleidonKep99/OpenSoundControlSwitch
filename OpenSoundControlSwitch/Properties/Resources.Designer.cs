55aea5c43b35f916cd1fa860174a6a1fcbc6c207
KaleidonKep99 <kaleidonkep99@outlook.com>
2022-08-03 11:36:20 +0200

    OpenSoundControlSwitch rename, damn Sourcetree went haywire here

--- OpenSoundControlSwitch/Properties/Resources.Designer.cs
+++ OpenSoundControlSwitch/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace OpenSoundControlSwitch.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VRChatOSCSwitch.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenSoundControlSwitch.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
